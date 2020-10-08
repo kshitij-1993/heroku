1ccf75b3d04289028dbc7cb8bfb6f7c85fe7c566
Kshitij <kshitijgupta1993@gmail.com>
2020-10-09 00:00:55 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>This is Going to be deployed on Heroku !</h1>
+      <h1>This is Going to be deployed on Heroku !!</h1>
+      <h2>Hello React World!</h2>
     </div>
   );
 }
