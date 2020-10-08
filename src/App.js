9427200bcdb89333455b64299e995714cb86f5e3
Kshitij <kshitijgupta1993@gmail.com>
2020-10-09 00:29:29 +0530

    Modified AppJS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Class2 from './Class2';
 
 function App() {
   return (
     <div className="App">
       <h1>This is Going to be deployed on Heroku !!</h1>
       <h2>Hello React World!</h2>
+      <Class2/>
     </div>
   );
 }
