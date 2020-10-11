1186c3cd232bc7ce8a7eae134f8bf331ef132952
Kshitij <kshitijgupta1993@gmail.com>
2020-10-11 14:34:39 +0530

    Added H2 to Dev Branch

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <h1>This is Going to be deployed on Heroku !!</h1>
-      <h2>Hello React World!</h2>
+      <h2>Dev Branch</h2>
       <Class2/>
     </div>
   );
